2f9d454eec90874da1e96ce686e95e5bb472031e
Danlin0829 <1249866122@qq.com>
2023-09-21 10:47:47 +0800

    website init

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-from flask import Flask, request, url_for, redirect
+from flask import Flask, request, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/index', methods=['POST'])
-def hello_world():  # put application's code here
-    key = request.args.get("key")
-    if key and not key.isspace():
-        return 'key is ' + key
-
-    return 'No key found'
-
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
